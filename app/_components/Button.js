ab6706df36c714fabcbdb71acbc99d2190cadaff
Whereisade <eniolaadeleke2303@gmail.com>
2024-11-11 14:20:24 +0100

    FEAT: fixed the button

--- app/_components/Button.js
+++ app/_components/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Button({ text, bordercolo, tcolo, bcolo, hov }) {
   return (
     <button
-      className={`py-[14px] px-[30px] hover:bg-blue-700 border ${hov} ${bcolo} ${tcolo} ${bordercolo} rounded-lg font-[500] relative w-full md:w-fit`}
+      className={`py-[14px] px-[30px] ${ hov ? 'hover:bg-black' : 'hover:bg-blue-700'} border ${bcolo} ${tcolo} ${bordercolo} rounded-lg font-[500] relative w-full md:w-fit`}
     >
       {text}
     </button>
